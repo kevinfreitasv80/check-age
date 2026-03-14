3cc2c2de82f6412f57c5287dda694e06ea6eaabd
Kevin Freitas <kevinfreitasv80@gmail.com>
2026-03-14 13:29:03 -0300

    new script

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano || fano.value < ano - 105) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+const yearInput = document.querySelector('#year');
+const txt = document.querySelector('.txt');
+const res = document.querySelector('#res');
+const radSex = document.querySelectorAll('.radSex');
+const btnCheck = document.querySelector('#btnVerificar');
+const img = document.querySelector('#image');
+
+const GENEROS = {
+    1: "Homem",
+    2: "Mulher"
+}
+
+function handleCheckAge() {
+    const date = new Date();
+    const year_actual = date.getFullYear();
+    const data_year = yearInput.value;
+
+    if (!data_year) return false;
+    if (data_year > year_actual) return false;
+    if (data_year < (year_actual - 105)) return false;
+
+    //window.alert('[ERRO] Verifique os dados e tente novamente!')
+    const idade = year_actual - data_year;
+
+    let genero;
+
+    if (radSex[0].checked) {
+        genero = GENEROS[1];
+        if (idade >= 0 && idade < 5) img.setAttribute('src' , './imgs/fotobh.png');
+        else if (idade <= 13) img.setAttribute('src', './imgs/fotoch.png');
+        else if (idade < 25) img.setAttribute('src', './imgs/fotojh.png');
+        else if (idade < 55) img.setAttribute('src', './imgs/fotoah.png');
+        else img.setAttribute('src', './imgs/fotoih.png');
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - fano.value 
-        let genero;
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 && idade < 5){
-                //Bebe
-                img.setAttribute('src' , 'fotobh.png')
-            } else if (idade <= 13) {
-                //Criança
-                img.setAttribute('src', 'fotoch.png')
-            } else if (idade < 25) {
-                //Jovem
-                img.setAttribute('src', 'fotojh.png')
-            } else if (idade < 55) {
-                //Adulto
-                img.setAttribute('src', 'fotoah.png')
-            } else {
-                //Idoso
-                img.setAttribute('src', 'fotoih.png')
-            }
-        } else if (fsex[1].checked){
-            genero = 'Mulher'
-            if (idade >=0 && idade < 5){
-                img.setAttribute('src', 'fotobm.png')
-            } else if (idade <= 13) {                
-                img.setAttribute('src', 'fotocm.png')
-            } else if (idade < 25) {    
-                img.setAttribute('src', 'fotojm.png')
-            } else if (idade < 55) {
-                img.setAttribute('src', 'fotoam.png')
-            } else {
-                img.setAttribute('src', 'fotoim.png')
-            }
-        }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos de idade`
-        res.appendChild(img)
+        genero = GENEROS[2];
+        if (idade >=0 && idade < 5) img.setAttribute('src', './imgs/fotobm.png')
+        else if (idade <= 13) img.setAttribute('src', './imgs/fotocm.png')
+        else if (idade < 25) img.setAttribute('src', './imgs/fotojm.png')
+        else if (idade < 55) img.setAttribute('src', './imgs/fotoam.png')
+        else img.setAttribute('src', 'fotoim.png')
+    }
+
+    txt.textContent = `Você é um(a) ${genero} com ${idade} anos de idade`;
+    if ("hidden" === img.classList[0]) {
+        img.classList.remove("hidden");
+        img.classList.add("block");
     }
 }
+
+btnCheck.addEventListener("click", handleCheckAge);
